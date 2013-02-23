5ec80eee99aa3ad8951cf63c7179a1625da02241
Pearce Merritt <pearcemerritt@gmail.com>
2013-02-23 12:05:11 -0800

    removed some left over pasted text in js/add_class.js and also added the beginnings of validation

--- js/add_class.js
+++ js/add_class.js
@@ -15,25 +15,6 @@ function isInputValid(elt) {
   return true;
 }
 
-// @return true if name input field is not just white space,
-//         false otherwise
-function isNameValid() {
-  return isInputValid( $('input[name=name]') );
-}
-
-// @return true if email input field is not just white space
-//         and contains @ and . in the right order,
-//         false otherwise
-function isEmailValid() {
-
-  var elt = $('input[name=email]');
-  if (!isInputValid(elt)) {
-    return false;
-  }
-  
-  return /^\w+@ucsc.edu$/.test(elt.val().trim());
-}
-
 // @return true if phone has 3 digits followed by 3 then 4
 //         more digits, false otherwise
 function isPhoneValid() {
@@ -44,6 +25,13 @@ function isPhoneValid() {
   }
 
   return /^.*\d\d\d.*\d\d\d.*\d\d\d\d.*$/.test(elt.val().trim());
+}
+
+// @return true if a dept has been chosen and false if the 
+//         default of "" has been chosen
+function isDeptValid() {
+
+  return $('select[name=dept]').val() !== '';
 }
 
 // Makes submit button click-able once all input elements have
@@ -60,60 +48,6 @@ function enableSubmitIfAllInputValid() {
 function generateValidationHintSpan(hintText) {
   return '<span class=\'inputHint\'>' + hintText + '</span>';
 }
-
-// Any time the name field in the form changes validate it
-$('input[name=name]').bind(
-  'change',
-  function() {
-
-    // If validation fails on name field, post a hint
-    if (!isNameValid()) {
-
-      // If hint not already present add one
-      if ($(this).next('span').length == 0) {
-        var hint = 'Please enter a name before continuing.';
-        $(this).after(generateValidationHintSpan(hint));
-      }
-    }
-    // Otherwise remove the hint and check to see if the whole
-    // form is validated and the submit button can be enabled
-    else {
-
-      // Remove previously added field hint since field now valid
-      if ($(this).next('span')) {
-        $(this).next('span').remove();
-      }
-      enableSubmitIfAllInputValid();
-    }
-  }
-);
-
-// Any time the email field in the form changes validate it
-$('input[name=email]').bind(
-  'change',
-  function() {
-
-    // If validation fails on email field, post a hint
-    if (!isEmailValid()) {
-
-      // If hint not already present add one
-      if ($(this).next('span').length == 0) {
-        var hint = 'Please enter a ucsc email (ending in @ucsc.edu) before continuing.';
-        $(this).after(generateValidationHintSpan(hint));
-      }
-    }
-    // Otherwise remove the hint and check to see if the whole
-    // form is validated and the submit button can be enabled
-    else {
-
-      // Remove previously added field hint since field now valid
-      if ($(this).next('span')) {
-        $(this).next('span').remove();
-      }
-      enableSubmitIfAllInputValid();
-    }
-  }
-);
 
 // Any time the phone field in the form changes validate it
 $('input[name=phone]').bind(
