a3b3806d1d0a6040ec9e3ba0b5ecb3ec2fb36556
Pearce Merritt <pearcemerritt@gmail.com>
2013-03-05 21:17:21 -0800

    got php script from stackOverflow that sends email through gmail smtp server and put it in send_gmail.php

---
+++ send_gmail.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+<!--=============================================================================-->
+<!-- end HTML5 boilerplate starter code =========================================-->
+<!--=============================================================================-->
+
+  <h1>UCSC Class Watcher</h1>
+
+<?php
+  require_once "Mail.php";
+
+  $from = "<ucscclasswatcher@gmail.com>";
+  $to = "<7076960269@txt.att.net>";
+  // $to = "<pearcemerritt@gmail.com>";
+  // $subject = "Hi!";
+  $body = "Class Message :)";
+
+  $host = "ssl://smtp.gmail.com";
+  $port = "465";
+  $username = "ucscclasswatcher@gmail.com";  //<> give errors
+  $password = 'wer234@#$';
+
+  $headers = array ('From' => $from, 'To' => $to);
+    //,'Subject' => $subject);
+
+  $smtp = Mail::factory('smtp',
+    array ('host' => $host,
+      'port' => $port,
+      'auth' => true,
+      'username' => $username,
+      'password' => $password));
+
+  $mail = $smtp->send($to, $headers, $body);
+
+  if (PEAR::isError($mail)) {
+    echo("<p>" . $mail->getMessage() . "</p>");
+  } 
+  else {
+    echo("<p>Message successfully sent!</p>");
+  }
+?>
+
+<!--===============================================================================-->
+<!-- start HTML5 boilerplate starter code =========================================-->
+<!--===============================================================================-->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
